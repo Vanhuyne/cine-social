9eafca2832e1acda2090673a70c89753b0801aac
Huy <106498928+Vanhuyne@users.noreply.github.com>
2025-01-13 21:54:58 +0700

    add test case for user service

--- backend/src/test/java/com/huy/backend/service/TestUserService.java
+++ backend/src/test/java/com/huy/backend/service/TestUserService.java
@@ -1,20 +1,36 @@
 package com.huy.backend.service;
 
+import com.huy.backend.dto.user.LoginRequest;
+import com.huy.backend.dto.user.LoginResponse;
 import com.huy.backend.dto.user.UserRegister;
+import com.huy.backend.dto.user.UserRegistrationResponseDTO;
 import com.huy.backend.exception.UserAlreadyExistsException;
+import com.huy.backend.models.RefreshToken;
+import com.huy.backend.models.User;
 import com.huy.backend.repository.UserRepo;
+import com.huy.backend.security.JwtUtil;
+import com.huy.backend.service.impl.RefreshTokenServiceImpl;
 import com.huy.backend.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class TestUserService {
@@ -28,24 +44,98 @@ public class TestUserService {
     @InjectMocks
     private UserServiceImpl userService;
 
-    // before testing
-     @BeforeEach
+    private UserRegister userRegister;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private RefreshTokenServiceImpl refreshTokenService;
+
+    @BeforeEach
     public void setUp() {
-         MockitoAnnotations.initMocks(this);
-     }
+        userRegister = UserRegister.builder()
+                .username("testUsername")
+                .email("testEmail")
+                .password("testPassword")
+                .build();
+    }
 
-     @Test
+    @Test
     void register_ShouldThrowException_WhenUsernameExists() {
-         // Given
-         UserRegister userRegister = new UserRegister("existingUser", "email@example.com", "password");
-         when(userRepo.existsByUsername("existingUser")).thenReturn(true);
+        // Given
+        when(userRepo.existsByUsername("testUsername")).thenReturn(true);
 
-         // When & Then
-         UserAlreadyExistsException exception = assertThrows(
-                 UserAlreadyExistsException.class,
-                 () -> userService.register(userRegister)
-         );
+        // THEN
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            // Action
+            userService.register(userRegister);
+        });
+    }
 
-         assertEquals("Username already exists", exception.getMessage());
-     }
+    @Test
+    void register_ShouldThrowException_WhenEmailExists() {
+        // Given
+        when(userRepo.existsByEmail("testEmail")).thenReturn(true);
+
+        // Action
+        assertThrows(UserAlreadyExistsException.class, () -> {
+            // Action
+            userService.register(userRegister);
+        });
+    }
+
+    @Test
+    void register_ShouldSaveUser_WhenUsernameAndEmailDoNotExist() {
+        // Given
+        when(userRepo.existsByUsername("testUsername")).thenReturn(false);
+        when(userRepo.existsByEmail("testEmail")).thenReturn(false);
+        when(userRepo.save(any(User.class))).thenReturn(new User());
+
+        // Action
+        UserRegistrationResponseDTO response = userService.register(userRegister);
+
+        // Assert
+        assertNotNull(response);
+        verify(userRepo, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void login_ShouldThrowException_WhenAccountDoesNotExist() {
+        // Given
+        when(userRepo.findByUsernameOrEmail("testUsername")).thenReturn(Optional.empty());
+
+        // Assert
+        assertThrows(UsernameNotFoundException.class, () -> {
+            // Action
+            userService.login(new LoginRequest("testUsername", "testPassword"));
+        });
+    }
+
+//    @Test
+//    void login_ShouldReturnLoginResponse_WhenCredentialsAreCorrect() {
+//        // Given
+//        User user = new User();
+//        when(userRepo.findByUsernameOrEmail("testUsername")).thenReturn(Optional.of(user));
+//        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+//                .thenReturn(new UsernamePasswordAuthenticationToken(user, "testPassword", Collections.emptyList()));
+//        when(jwtUtil.generateToken(anyString(), anySet())).thenReturn("accessToken");
+//        when(refreshTokenService.createOrUpdateRefreshToken(any(User.class))).thenReturn(new RefreshToken(1L, user, "refreshToken", Instant.now()));
+//
+//        // Action
+//        LoginResponse response = userService.login(new LoginRequest("testUsername", "testPassword"));
+//        System.out.println("accessToken " + response.getAccessToken());
+//        System.out.println("refreshToken " + response.getRefreshToken());
+//        System.out.println("message " + response.getMessage());
+//
+//
+//        // Assert
+//        assertNotNull(response);
+//        assertEquals("Login successful", response.getMessage());
+//        assertEquals("accessToken", response.getAccessToken());
+//        assertEquals("refreshToken", response.getRefreshToken());
+//    }
 }
